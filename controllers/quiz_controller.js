108f3522b760589154ea2042a2e6ef737b1011da
EMM <eliasmoli@outlook.com>
2015-07-25 20:39:57 +0200

    Listado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,31 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
+// GET /quizes
+exports.index = function(req, res) {
 	// Se utiliza done(), porque success() está deprecated
-	models.Quiz.findAll().done(function(quiz){
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	// O usar then
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes});
 	})
 };
 
-// GET /quizes/answer
+exports.show = function(req, res) {
+   // con find() no funciona
+   models.Quiz.findById(req.params.quizId).then(function(quiz) {
+      res.render('quizes/show', { quiz: quiz});
+   })
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	// Se utiliza done(), porque success() está deprecated
-   	models.Quiz.findAll().done(function(quiz){
-   		if (req.query.respuesta === quiz[0].respuesta){
-   			res.render('quizes/answer', {respuesta: 'Correcto'});
+	// con find() no funciona
+   	models.Quiz.findById(req.params.quizId).then(function(quiz){
+   		if (req.query.respuesta === quiz.respuesta){
+   			res.render('quizes/answer',
+   				{ quiz: quiz, respuesta: 'Correcto'});
    		} else {
-      		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+      		res.render('quizes/answer',
+      			{ quiz: quiz, respuesta: 'Incorrecto'});
    		}
    	})
 };
