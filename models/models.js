9e8afa36df5bde5396582d06330a936c889a3fe3
EMM <eliasmoli@outlook.com>
2015-07-25 19:07:36 +0200

    Despliegue PostgreSQL en Heroku

---
+++ models/models.js
@@ -0,0 +1,49 @@
+var path = require('path');
+
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite o Postgres:
+var sequelize = new Sequelize(DB_name, user, pwd,
+                      { dialect:  protocol,
+                        protocol: protocol,
+                        port:     port,
+                        host:     host,
+                        storage:  storage, // SQLite
+                        omitNull: true // Pg
+                      }
+                    );
+
+// Importar la definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+exports.Quiz = Quiz; // exportar tabla Quiz
+
+// sequelize.sync() inicializa tabla de preguntas en DB
+// Se utiliza done(), porque success() está deprecated
+sequelize.sync().then(function() {
+  // then() ejecuta el manejador despues del callback
+  Quiz.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',
+      	            respuesta: 'Roma'
+      	         })
+      .then(function(){console.log('Base de datos inicializada')});
+    };
+  });
+});
