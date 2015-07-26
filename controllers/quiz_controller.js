e7e25f728a4ed0065ea9b05bffa0dec668eec6b8
EMM <eliasmoli@outlook.com>
2015-07-26 17:32:19 +0200

    Formulario de busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,10 +17,24 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
 	// Se utiliza done(), porque success() está deprecated
 	// O usar then
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
+   // req.query.search != undefined || req.query.search != null
+   if (req.query.search) {
+      var filtro  = "%" + req.query.search + "%";
+      //.replace(" ", "%");
+      filtro = filtro.replace(/\s/g, "%");
+      //(req.query.search || '')
+      //the ASC keyword orders by the specified columns alphabetically.
+      // P.e. Si se busca "de gal" en el formulario, se ejecuta:
+      // SELECT `id`, `pregunta`, `respuesta`, `createdAt`, `updatedAt` FROM `Quizzes` AS `Quiz` WHERE pregunta like '%de%gal%' ORDER BY pregunta ASC;
+      models.Quiz.findAll({where: ["pregunta like ?", filtro ],order:'pregunta ASC'}).then(function(quizes){
+         res.render('quizes/index.ejs', {quizes: quizes});
    }).catch(function(error) { next(error);})
-};
+   } else {
+      models.Quiz.findAll().then(function(quizes) {
+         res.render('quizes/index.ejs', { quizes: quizes});
+      }).catch(function(error) { next(error);})
+   };
+}
 
 exports.show = function(req, res) {
    res.render('quizes/show', { quiz: req.quiz});
