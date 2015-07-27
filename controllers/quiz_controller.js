95fd838be819a57a3732253928b5e50ea6631ca5
EMM <eliasmoli@outlook.com>
2015-07-27 20:37:17 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,19 +25,22 @@ exports.index = function(req, res) {
       //(req.query.search || '')
       //the ASC keyword orders by the specified columns alphabetically.
       // P.e. Si se busca "de gal" en el formulario, se ejecuta:
-      // SELECT `id`, `pregunta`, `respuesta`, `createdAt`, `updatedAt` FROM `Quizzes` AS `Quiz` WHERE pregunta like '%de%gal%' ORDER BY pregunta ASC;
-      models.Quiz.findAll({where: ["pregunta like ?", filtro ],order:'pregunta ASC'}).then(function(quizes){
-         res.render('quizes/index.ejs', {quizes: quizes});
-   }).catch(function(error) { next(error);})
+      // SELECT `id`, `pregunta`, `respuesta`, `createdAt`, 
+      //`updatedAt` FROM `Quizzes` AS `Quiz` WHERE pregunta like '%de%gal%' 
+      //ORDER BY pregunta ASC;
+      models.Quiz.findAll({where: ["pregunta like ?", filtro ],
+                                    order:'pregunta ASC'}).then(function(quizes){
+         res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+   }).catch(function(error) { next(error)});
    } else {
       models.Quiz.findAll().then(function(quizes) {
-         res.render('quizes/index.ejs', { quizes: quizes});
+         res.render('quizes/index.ejs', { quizes: quizes, errors: []});
       }).catch(function(error) { next(error);})
    };
 }
 
 exports.show = function(req, res) {
-   res.render('quizes/show', { quiz: req.quiz});
+   res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -46,22 +49,29 @@ exports.answer = function(req, res) {
    if (req.query.respuesta === req.quiz.respuesta) {
       resultado = 'Correcto';
    }
-   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
 exports.new = function(req, res) {
    var quiz = models.Quiz.build(
       {pregunta: "Pregunta", respuesta: "Respuesta"});
-   res.render('quizes/new', {quiz: quiz});
+   res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
    var quiz = models.Quiz.build( req.body.quiz );
-   // guarda en DB los campos pregunta y respuesta de quiz
-   quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-      res.redirect('/quizes');})   // res.redirect: Redirección HTTP a lista de preguntas
+   quiz.validate().then(
+      function(err){
+         if (err) {
+            res.render('quizes/new', {quiz: quiz, errors: err.errors});
+         } else {
+            // guarda en DB los campos pregunta y respuesta de quiz
+            quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+            res.redirect('/quizes');})   // res.redirect: Redirección HTTP a lista de preguntas
+         }
+      });
 };
 
 // GET /author
