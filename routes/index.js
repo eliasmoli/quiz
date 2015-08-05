9f781c5fbb7d9b3a025c2e23892e21ea08ee8941
EMM <eliasmoli@outlook.com>
2015-08-05 11:18:52 +0200

    Estadísticas

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,6 +29,8 @@ router.post('/quizes/create',              sessionController.loginRequired, quiz
 router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
+
+router.get('/quizes/statistics', 		   statisticsController.statistics);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
